4b38b42a040dab82ec8200e0b79492845db77613
SabaMuladze <muladze66@bk.ru>
2023-10-25 22:43:44 +0400

    style:add some changes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
-<script >
-</script>
 
-<template></template>
+<template>
+    <div class="flex flex-col w-full min-h-screen items-center">
+        <div class="mt-20 px-12 w-full flex flex-col gap-8 items-center">
+            <p class="text-[25px] font-bold flex">PING<span class="text-[#4C7BF3] h-1 w-1 ">.</span></p>
+            <div class="flex flex-col gap-3">
+                <h2 class="text-[26px] text-[#969696] font-light">We are launching <strong class="text-black">soon!</strong>
+                </h2>
+                <p class="font-thin flex justify-center">Subscribe and get notified</p>
+            </div>
+            <div class="w-full flex flex-col gap-[10px]">
+                <div class="w-full h-10 rounded-3xl">
+                    <input type="email" placeholder="Your email address…">
+                </div>
+                <button class="w-full h-10 bg-[#4C7BF3] rounded-3xl font-semibold text-white">Notify Me</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script >
+
+</script>
 
 <style></style>
